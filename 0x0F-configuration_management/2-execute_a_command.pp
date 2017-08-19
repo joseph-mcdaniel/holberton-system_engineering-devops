fbb88bec4c76c6152af9fd8b64c8c08df788499c
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-08-19 01:11:50 +0000

    add path

--- 0x0F-configuration_management/2-execute_a_command.pp
+++ 0x0F-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # manifest that kills a process"
-$filename = /home/vagrant/holberton-system_engineering-devops/0x05-processes_and_signals/4-to_infinity_and_beyond
-exec { '$(filename)':
-  command => 'pkill -9 $(filename)'
+exec { 'killmennow':
+  command => "pkill -f 'killmenow'",
+  path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
