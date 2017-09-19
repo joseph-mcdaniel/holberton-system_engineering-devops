9ac7dfba60399630ee149259692dd16d6c750e15
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-09-19 06:05:01 +0000

    gather data from api

---
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Gather data from REST API """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    users = requests.get("{}/users?id={}".format(url, sys.argv[1])).json()
+    todos = requests.get("{}/todos?userId={}".format(url, sys.argv[1])).json()
+    tasks = 0
+
+    for i in range(len(todos)):
+        if (todos[i].get("completed")):
+            tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(users[0].get("name"),
+                                                          tasks,
+                                                          len(todos)))
+    for j in todos:
+        print("\t {}".format(j.get("title")))
