bb04560551523f42b1fdfea4934b70faa5c7b374
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-09-19 06:52:35 +0000

    add uid variable

--- 0x16-api/1-export_to_CSV.py
+++ 0x16-api/1-export_to_CSV.py
@@ -7,14 +7,15 @@ import sys
 
 
 if __name__ == "__main__":
+    uid = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com"
-    users = requests.get("{}/users?id={}".format(url, sys.argv[1])).json()
-    todos = requests.get("{}/todos?userId={}".format(url, sys.argv[1])).json()
+    users = requests.get("{}/users?id={}".format(url, uid)).json()
+    todos = requests.get("{}/todos?userId={}".format(url, uid)).json()
 
-    with open('{}.csv'.format(sys.argv[1]), "w", newline='\n') as csvfile:
+    with open('{}.csv'.format(uid), "w", newline='\n') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
         for i in todos:
-            writer.writerow([sys.argv[1],
+            writer.writerow([uid,
                             users[0].get("username"),
                             i.get("completed"),
                             i.get("title")])
